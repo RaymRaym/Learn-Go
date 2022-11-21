058d9cc85f1a4387da64747404dc97e32a34d699
RaymRaym <wrhlh369@gmail.com>
2022-11-21 10:28:02 +0800

    append

--- basic/useArray.go
+++ basic/useArray.go
@@ -16,4 +16,23 @@ func main() {
 	}
 
 	fmt.Println(123123)
+
+	// 被append的数组必须是[]，而非[3]or[...]
+	// append 必须要有返回值
+	var y = []int{1, 2, 3}
+	y = append(y, 4)
+
+	var x []int
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 10)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 20)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 30)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 40)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 50)
+	fmt.Println(x, len(x), cap(x))
+
 }
